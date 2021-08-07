62b5995134be9bf0a896ae21911f55ddabc640e3
yamamoto.ikutaro <s-sxbx.37@outlook.jp>
2021-08-07 09:54:35 +0000

    formスタイル修正

--- resources/views/task/taskRegistration.blade.php
+++ resources/views/task/taskRegistration.blade.php
@@ -27,7 +27,7 @@
         </div>
         <div class="form-group">
             <div class="d-flex">
-                <div class="text-right" style="width:100px;">
+                <div class = "text-right" style = "width:100px;">
                     <p>担当者：</p>
                 </div>
                 <div>
